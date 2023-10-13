cde50e4df65ee4a7fdd8bc937fd3f3217fd24cf3
Santoshkumar70 <142250832+Santoshkumar70@users.noreply.github.com>
2023-10-13 15:44:23 +0530

    Reverse String prpgram

---
+++ Reverse.py
@@ -0,0 +1,7 @@
+string = "Hey Hello Good Morning"
+sp = string.split()
+
+print(sp[0],sp[3],sp[1],sp[2])
+print(sp[1],sp[0],sp[3],sp[2])
+print(sp[3],sp[1],sp[2],sp[0])
+print(sp[2],sp[0],sp[3],sp[1])
